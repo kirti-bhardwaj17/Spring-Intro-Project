0f03caf8aeffe042e2667de2e3eacdd5f4a21fde
kirti-bhardwaj17 <bhardwajkirti17@gmail.com>
2025-03-01 14:21:04 +0530

    Using GET Request Method and passing name as query parameter

--- src/main/java/com/example/demo/controller/HelloController.java
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -1,15 +1,20 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/hello") // Base URL: http://localhost:8080/hello
 public class HelloController {
 
+    // Default GET request (http://localhost:8080/hello)
     @GetMapping
     public String sayHello() {
         return "Hello from BridgeLabz!";
+    }
+
+    // GET request with a query parameter (http://localhost:8080/hello/query?name=Mark)
+    @GetMapping("/query")
+    public String greetUser(@RequestParam String name) {
+        return "Hello " + name + " from BridgeLabz!";
     }
 }
